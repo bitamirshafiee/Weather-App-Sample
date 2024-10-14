9d5a7007321568e90b109e82fe543a22dd53baae
mirshafiee <b.mirshafiee90@gmail.com>
2024-10-14 21:11:17 +0200

    Refactor theme name

--- app/src/main/java/com/weatherappsample/ui/theme/Theme.kt
+++ app/src/main/java/com/weatherappsample/ui/theme/Theme.kt
@@ -33,7 +33,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun WeatherAppTheme(
+fun WeatherTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
