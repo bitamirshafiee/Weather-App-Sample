7283002d9c4bf8c18edd3c0e7c7db489c73ba995
mirshafiee <b.mirshafiee90@gmail.com>
2024-10-14 21:14:27 +0200

    Adding navigation and also routes to screens

---
+++ app/src/main/java/com/weatherappsample/ui/WeatherAppState.kt
@@ -0,0 +1,33 @@
+package com.weatherappsample.ui
+
+import android.content.Context
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun rememberWeatherAppState(
+    navController: NavHostController = rememberNavController(),
+    context: Context = LocalContext.current
+) = remember(navController, context) {
+    WeatherAppState(navController)
+}
+
+class WeatherAppState(
+    val navController: NavHostController
+) {
+    fun navigateTo(route: String) {
+        navController.navigate(route = route)
+    }
+    fun navigateBack() {
+        navController.popBackStack()
+    }
+}
+
+sealed class NavControllerRoute(
+    val route: String,
+) {
+    data object WeatherList : NavControllerRoute("weatherList")
+}
