927cd39d2d6b650fa25ce50e0b20ae6be5b5f8f1
mirshafiee <b.mirshafiee90@gmail.com>
2024-10-14 20:33:48 +0200

    Rename theme

--- app/src/main/java/com/weatherappsample/ui/theme/Theme.kt
+++ app/src/main/java/com/weatherappsample/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.weatherappsample.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -34,7 +33,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun WeatherAppSampleTheme(
+fun WeatherAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
