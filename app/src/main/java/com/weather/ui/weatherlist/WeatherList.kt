0c8a677d46204679faa2fd187801e9301a1aaebb
mirshafiee <b.mirshafiee90@gmail.com>
2024-10-14 22:46:59 +0200

    Add weather details with a sample item.

---
+++ app/src/main/java/com/weather/ui/weatherlist/WeatherList.kt
@@ -0,0 +1,82 @@
+package com.weather.ui.weatherlist
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.weather.R
+
+@Composable
+fun WeatherList() {
+
+}
+
+@Composable
+fun WeatherItem(modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp), verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            modifier = Modifier
+                .size(80.dp),
+            contentScale = ContentScale.FillBounds,
+            contentDescription = null
+        )
+        Text(
+            modifier = Modifier.padding(horizontal = 8.dp),
+            text = "Stockholm",
+            overflow = TextOverflow.Ellipsis,
+            fontWeight = FontWeight.Bold,
+            style = MaterialTheme.typography.titleLarge
+        )
+        Column(modifier = Modifier.weight(1f)) {
+            Temperatures()
+        }
+    }
+}
+
+@Composable
+private fun Temperatures() {
+    Text(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 8.dp),
+        text = "18",
+        overflow = TextOverflow.Ellipsis,
+        style = MaterialTheme.typography.titleLarge,
+        textAlign = TextAlign.Center
+    )
+    Text(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 8.dp),
+        text = "18",
+        overflow = TextOverflow.Ellipsis,
+        style = MaterialTheme.typography.titleLarge,
+        textAlign = TextAlign.Center
+    )
+}
+
+@Composable
+@Preview
+fun WeatherItemPreview() {
+    WeatherItem()
+}
+
