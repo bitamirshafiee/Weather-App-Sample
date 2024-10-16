be54d5ee3a95389a20c094bec1225d8a9d7477f4
mirshafiee <b.mirshafiee90@gmail.com>
2024-10-16 14:04:22 +0200

    Added needed services

--- app/src/main/java/com/weather/data/service/WeatherService.kt
+++ app/src/main/java/com/weather/data/service/WeatherService.kt
@@ -1,4 +1,17 @@
 package com.weather.data.service
 
+import com.weather.const.APP_ID
+import com.weather.const.TEMPERATURE_UNIT
+import com.weather.data.model.WeatherResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface WeatherService {
+
+    @GET("weather?")
+    suspend fun getWeather(
+        @Query("q") cityName: String,
+        @Query("appid") appId: String = APP_ID,
+        @Query("units") temperatureUnit: String = TEMPERATURE_UNIT
+    ): WeatherResponse
 }
