9cd6304f42ef1937a33c3b58d4965c80a24d9a2b
mirshafiee <b.mirshafiee90@gmail.com>
2024-10-16 15:02:47 +0200

    Add weather list to the composable routes

--- app/src/main/java/com/weather/ui/WeatherApp.kt
+++ app/src/main/java/com/weather/ui/WeatherApp.kt
@@ -3,6 +3,7 @@ package com.weather.ui
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.weather.ui.weatherlist.WeatherList
 
 @Composable
 fun WeatherApp(appState: WeatherAppState = rememberWeatherAppState()) {
@@ -11,7 +12,7 @@ fun WeatherApp(appState: WeatherAppState = rememberWeatherAppState()) {
         startDestination = NavControllerRoute.WeatherList.route
     ) {
         composable(route = NavControllerRoute.WeatherList.route) {
-            //TODO: Weather list
+            WeatherList()
         }
     }
 }
