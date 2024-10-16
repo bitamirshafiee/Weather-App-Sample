6be753fe674f3f2d1eb8bfc2a241a9bf18fbd3f9
mirshafiee <b.mirshafiee90@gmail.com>
2024-10-16 14:54:46 +0200

    Integrating service provider with repository and make async api calls

--- app/src/main/java/com/weather/data/repository/WeatherRepository.kt
+++ app/src/main/java/com/weather/data/repository/WeatherRepository.kt
@@ -1,4 +1,26 @@
 package com.weather.data.repository
 
-class WeatherRepository {
+import com.weather.data.model.WeatherResponse
+import com.weather.data.service.ServiceProvider
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+
+abstract class WeatherRepository {
+    abstract suspend fun getCurrentWeathersByCityNames(): List<Deferred<WeatherResponse>>
+}
+
+class WeatherRepositoryImpl(serviceProvider: ServiceProvider) : WeatherRepository() {
+
+    private val weatherService = serviceProvider.weatherService
+
+    //better using city id with search in json file
+    //TODO: an idea of how it should be implemented with async adding dispatchers to scope
+    override suspend fun getCurrentWeathersByCityNames(): List<Deferred<WeatherResponse>> =
+        coroutineScope {
+            val deferredResult1 = async { weatherService.getWeather("Stockholm") }
+            val deferredResult2 = async { weatherService.getWeather("London") }
+            listOf(deferredResult1, deferredResult2)
+        }
+
 }
